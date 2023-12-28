e907ee541a49a6f19bf5cf87dc9ab21ae2b19c8d
Brendan Lawless <brendanlawless@hotmail.com>
2023-12-28 21:21:13 +0000

    Init

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
+import styles from '@/app/ui/home.module.css';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -24,8 +26,12 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div className={styles.shape}></div>
+          <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+            {/* Add Hero Images Here */}
+          </div>
         </div>
       </div>
     </main>
